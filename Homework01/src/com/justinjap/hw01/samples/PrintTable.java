6ce945b782e338abffed1278e53871736c5ccb27
Justin Jap <jusdan88@gmail.com>
2014-05-20 20:16:44 -0700

    Moved gitignore to top level. Removed compiled class files. Added DisplayJAVAPattern.

--- Homework01/src/com/justinjap/hw01/samples/PrintTable.java
+++ Homework01/src/com/justinjap/hw01/samples/PrintTable.java
@@ -4,10 +4,10 @@ public class PrintTable {
 
 	public static void main(String[] args) {
 		// Display A Table
-		System.out.println("a a^2 a^3");
-		System.out.println("1 1 1");
-		System.out.println("2 4 4");
-		System.out.println("3 9 27");
-		System.out.println("4 16 64");
+		System.out.println("a    a^2   a^3");
+		System.out.println("1    1     1");
+		System.out.println("2    4     4");
+		System.out.println("3    9     27");
+		System.out.println("4    16    64");
 	}
 }
